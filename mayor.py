af7155eb71245663a21d5814b2c062a3a9904557
Maria Fernanda Alvarez Aguirre <000405195@bga.upb>
2019-03-01 13:16:32 -0500

    Mi primer commit

---
+++ mayor.py
@@ -0,0 +1,17 @@
+num=int(input('Ingrese el número'))
+
+if num == 0:
+
+  print('El número es cero')
+
+else:
+
+ if num>0:
+
+  print('El número es mayor que cero')
+
+ else:
+
+   if num<0:
+
+    print('El número es menor que cero')
