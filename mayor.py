1b989c937955d9f9cf2b51090f8eb8a215be2e49
Maria Fernanda Alvarez Aguirre <000405195@bga.upb>
2019-03-01 14:21:29 -0500

    Ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,17 +1,20 @@
-num=int(input('Ingrese el número'))
+contadorpositivo=0
+contadornegativo=0
+contadorcero=0
+for n in range(1,11):
+    n=int(input('Ingrese los número'))
+    if n>0:
+        contadorpositivo=contadorpositivo+1
+    elif n<0:
+        contadornegativo=contadornegativo+1
+    else:
+        contadorcero=contadorcero+1
+print(contadorpositivo)
+print(contadornegativo)
+print(contadorcero)
 
-if num == 0:
 
-  print('El número es cero')
 
-else:
 
- if num>0:
 
-  print('El número es mayor que cero')
 
- else:
-
-   if num<0:
-
-    print('El número es menor que cero')
